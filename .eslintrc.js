82b1bb5455d01e464cadda3c6b4083b13fddd1ca
salty-max <max@jellycat.fr>
2023-07-19 01:12:06 +0200

    feat: implements basic idea

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.json', '.prettierrc'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'jest'],
+  rules: {
+    'prettier/prettier': 0,
+    'jest/no-disabled-tests': 0,
+  },
+}
