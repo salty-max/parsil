e56c7883fdcab75eab4f14646e77c93a13db5fcb
salty-max <max@jellycat.fr>
2023-07-19 02:03:36 +0200

    feat: refactors code to implement Parser class + tests

---
+++ src/sequence-of/sequence-of.spec.ts
@@ -0,0 +1,29 @@
+import { str } from '../str/str'
+import { sequenceOf } from './sequence-of'
+
+describe('sequenceOf', () => {
+  it('parser state should have an array of embedded parsers results as result', () => {
+    const parser = sequenceOf([str('foo'), str('bar')])
+    const res = parser.run('foobar')
+
+    expect(res).toStrictEqual({
+      targetString: 'foobar',
+      index: 6,
+      result: ['foo', 'bar'],
+      isError: false,
+      error: null,
+    })
+  })
+  it('parser should stop parsing if an error is present', () => {
+    const parser = sequenceOf([str('foo'), str('bar')])
+    const res = parser.run('foo bar')
+
+    expect(res).toStrictEqual({
+      targetString: 'foo bar',
+      index: 3,
+      result: ['foo', null],
+      isError: true,
+      error: `str: Tried to match 'bar', but got ' bar'`,
+    })
+  })
+})
---
+++ src/str/str.spec.ts
@@ -0,0 +1,37 @@
+import { str } from './str'
+
+describe('str', () => {
+  it('should succeed when match is found', () => {
+    const parser = str('foo')
+    const res = parser.run('foo')
+    expect(res).toStrictEqual({
+      targetString: 'foo',
+      index: 3,
+      result: 'foo',
+      isError: false,
+      error: null,
+    })
+  })
+  it('should update state error when match is not found', () => {
+    const parser = str('foo')
+    const res = parser.run('bar')
+    expect(res).toStrictEqual({
+      targetString: 'bar',
+      index: 0,
+      result: null,
+      isError: true,
+      error: `str: Tried to match 'foo', but got 'bar'`,
+    })
+  })
+  it('should update state error when end of input is found instead of match', () => {
+    const parser = str('foo')
+    const res = parser.run('')
+    expect(res).toStrictEqual({
+      targetString: '',
+      index: 0,
+      result: null,
+      isError: true,
+      error: `str: Tried to match 'foo', but got unexpected end of input`,
+    })
+  })
+})
