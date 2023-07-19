d9f2964b4a112165f1deb3f1ce15b47eb04a6232
salty-max <max@jellycat.fr>
2023-07-20 01:37:18 +0200

    feat: rearranges exports and strongly types choice and sequenceOf

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,71 @@
-export * from './parsers'
+import { InputType, InputTypes } from './input-types'
+import {
+  Err,
+  Ok,
+  Parser,
+  ParserState,
+  ResultType,
+  updateError,
+  updateResult,
+  updateState,
+} from './parser'
+import {
+  between,
+  anyChar,
+  char,
+  choice,
+  digits,
+  fail,
+  letters,
+  many,
+  manyOne,
+  recursive,
+  regex,
+  sepBy,
+  sepByOne,
+  sequenceOf,
+  str,
+} from './parsers'
+import {
+  decoder,
+  encoder,
+  getCharacterLength,
+  getNextCharWidth,
+  getString,
+  getUtf8Char,
+} from './util'
+
+export type { ParserState, ResultType, Err, Ok, InputType }
+
+const P = {
+  between,
+  anyChar,
+  char,
+  choice,
+  digits,
+  fail,
+  letters,
+  many,
+  manyOne,
+  recursive,
+  regex,
+  sepBy,
+  sepByOne,
+  sequenceOf,
+  str,
+}
+
+export default P
+export {
+  encoder,
+  decoder,
+  Parser,
+  updateError,
+  updateState,
+  updateResult,
+  InputTypes,
+  getCharacterLength,
+  getNextCharWidth,
+  getString,
+  getUtf8Char,
+}
