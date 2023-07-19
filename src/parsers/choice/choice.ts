e66bb892dff281ef63019fcf81d25e6eb4bf556b
salty-max <max@jellycat.fr>
2023-07-19 15:44:16 +0200

    feat: implements choice parser

---
+++ src/parsers/choice/choice.ts
@@ -0,0 +1,30 @@
+import { Parser, ParserState } from '../../parser'
+
+/**
+ * Tries to parse input with the first parser in a list that succeeds.
+ * If all parsers fail, it fails with the last and furthest error.
+ *
+ * @param parsers An array of parsers to apply to the input.
+ * @throws Error If the parsers array is empty.
+ * @returns A new Parser instance that will try each parser in order until one succeeds.
+ */
+export const choice = (parsers: Array<Parser<any>>): Parser<any> => {
+  if (parsers.length === 0)
+    throw new Error('choice requires a non-empty list of parsers')
+
+  return new Parser((state): ParserState<any, any> => {
+    if (state.isError) return state
+
+    let error = null
+    for (const p of parsers) {
+      const out = p.p(state)
+      if (!out.isError) return out
+
+      if (error === null || (error && out.index > error.index)) {
+        error = out
+      }
+    }
+
+    return error as ParserState<any, any>
+  })
+}
