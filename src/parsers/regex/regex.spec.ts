b859de08ba63c4c7ad38e3011144a88a1de6140a
salty-max <max@jellycat.fr>
2023-07-20 00:32:23 +0200

    feat: adds regex tests

---
+++ src/parsers/regex/regex.spec.ts
@@ -0,0 +1,58 @@
+import { regex } from './regex' // Replace with your actual module path
+
+describe('regex', () => {
+  it('should correctly match the input against the regex pattern', () => {
+    const pattern = /^[A-Za-z]+$/
+    const input = 'abc'
+    const parser = regex(pattern)
+    const result = parser.run(input)
+
+    expect(result).toEqual({
+      index: 3,
+      result: 'abc',
+      isError: false,
+    })
+  })
+
+  it('should return an error when the input does not match the regex pattern', () => {
+    const pattern = /^\d+$/
+    const input = 'abc'
+    const parser = regex(pattern)
+    const result = parser.run(input)
+
+    expect(result).toEqual({
+      index: 0,
+      isError: true,
+      error:
+        "ParseError @ index 0 -> regex: Tried to match /^\\d+$/, got 'abc...'",
+    })
+  })
+
+  it('should return an error when the input string is empty', () => {
+    const pattern = /^\d+$/
+    const input = ''
+    const parser = regex(pattern)
+    const result = parser.run(input)
+
+    expect(result).toEqual({
+      index: 0,
+      isError: true,
+      error:
+        'ParseError @ index 0 -> regex: Tried to match /^\\d+$/, but got unexpected end of input',
+    })
+  })
+
+  it('should throw an error if the pattern is not a RegExp', () => {
+    const pattern = 'abc'
+    expect(() => regex(pattern as unknown as RegExp)).toThrow(
+      'regex must be called with a Regular Expression, but got [object String]'
+    )
+  })
+
+  it('should throw an error if the pattern does not contain a start assertion (^)', () => {
+    const pattern = /abc/
+    expect(() => regex(pattern)).toThrow(
+      "regex parsers must contain '^' start assertion"
+    )
+  })
+})
