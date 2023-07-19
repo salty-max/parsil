f5fb7e83edd837a8204469020cb9b8bd204f6268
salty-max <max@jellycat.fr>
2023-07-19 15:44:16 +0200

    feat: implements choice parser

---
+++ src/parsers/choice/choice.spec.ts
@@ -0,0 +1,28 @@
+import { assertIsError, assertIsOk } from '../../util'
+import { digits } from '../digits'
+import { letters } from '../letters'
+import { choice } from './choice'
+
+describe('choice', () => {
+  it('should return result of first successful parser', () => {
+    const parser = choice([digits, letters])
+    const result = parser.run('foo')
+
+    assertIsOk(result)
+    expect(result.result).toBe('foo')
+  })
+
+  it('should return error if all parsers fail', () => {
+    const parser = choice([digits, letters])
+    const result = parser.run('@')
+
+    assertIsError(result)
+    expect(result.error).toBe(`ParseError (position: 0): Expected digits`)
+  })
+
+  test('It should throw error if parsers array is empty', () => {
+    expect(() => choice([])).toThrow(
+      'choice requires a non-empty list of parsers'
+    )
+  })
+})
