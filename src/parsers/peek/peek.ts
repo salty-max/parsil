611ea68e8d34c67e06bd4bb1c2ed25315ede9682
salty-max <max@jellycat.fr>
2023-07-21 01:30:17 +0200

    chore: adds doc for peek

--- src/parsers/peek/peek.ts
+++ src/parsers/peek/peek.ts
@@ -1,5 +1,16 @@
 import { Parser, updateError, updateState } from '../../parser/parser'
 
+/**
+ * `peek` peeks (u don't say) at the next byte in the input, without consuming it.
+ * It returns the value of the next byte as a number.
+ *
+ * @example
+ * const parser = P.peek;
+ * const data = new Uint8Array([1, 2, 3]);
+ * parser.run(new DataView(data.buffer)); // returns { isError: false, result: 1, index: 0 }
+ *
+ * @returns {Parser<number>} A parser that peeks at the next byte in the input.
+ */
 export const peek: Parser<number> = new Parser((state) => {
   if (state.isError) return state
 
